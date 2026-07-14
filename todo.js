a9628ad98436e36f18612a16e9c770c857d7f624
XelilovTh <tehmazxelilov7@gmail.com>
2026-07-14 22:56:38 +0400

    todo list v2

--- todo.js
+++ todo.js
@@ -1,37 +1,32 @@
-const inp = document.getElementById("input");
-const addBtn = document.getElementById("addBtn");
-const ul = document.querySelector("ul");
+const inp = document.querySelector("#input");
+const addBtn = document.querySelector(".addBtn");
+const list = document.querySelector("ul");
 
-addBtn.addEventListener("click" , (e)=>{
+addBtn.addEventListener("click", (e) => {
+  const text = inp.value.trim();
+  if (text === "") return;
 
-    const text = inp.value.trim();
-    if(text==="") return;
+  const li = document.createElement("li");
+  const removeBtn = document.createElement("button");
+  const div = document.createElement("div");
 
-    const li = document.createElement("li");
-    li.textContent = text;
+  li.textContent = text;
+  removeBtn.textContent = "✖";
+  li.classList.add("elementText");
+  removeBtn.classList.add("removeBtn");
+  div.classList.add("listElement")
+  div.appendChild(li);
+  div.appendChild(removeBtn);
+  list.appendChild(div)
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "🗑️";
+  div.addEventListener("click", (e) => {
+    if (e.target.classList.contains("elementText")) {
+      li.classList.toggle("done");
+    }
+    if (e.target.classList.contains("removeBtn")) {
+      e.target.parentElement.remove();
+    }
+  });
 
-    const taskDiv = document.createElement("div");
-    taskDiv.classList.add("taskDiv")
-
-    ul.appendChild(taskDiv);
-    taskDiv.appendChild(li);
-    taskDiv.appendChild(deleteBtn)
-
-
-
-    li.classList.add("listElement");
-    deleteBtn.classList.add("remove");
-
-    li.addEventListener("click" , (e)=>{
-        e.currentTarget.classList.toggle("done")
-    })
-    deleteBtn.addEventListener("click" , (e)=>{
-        e.target.parentElement.remove();
-    })
-
-    inp.value=""
-    
-} )
+  inp.value=""
+});
